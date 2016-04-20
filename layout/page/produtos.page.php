a3375c62f3a9ad04d71cc25af673f3114c230510
Guilherme Martins Panayotou <guilherme.it@live.com>
2016-04-20 12:27:20 -0300

    base pronta, falta apenas os detalhes...

--- layout/page/produtos.page.php
+++ layout/page/produtos.page.php
@@ -9,25 +9,32 @@
     foreach ($produtos as $p):
         $cont++;
         ?>
-    <div class="coluna2 <?php if($cont % 2 == 0) echo 'fr'; else echo 'fl'; ?>" style="margin: 5px auto;">
-        <div style="width: 60%;">
-            <h2><?php echo $p->getNome(); ?></h2>
-            <small>
-                <?php
-                foreach ($categorias as $cat) {
-                    if ($cat->getId() == $p->getIdcategoria()) {
-                        echo $cat->getNome();
-                        break;
+        <div class="coluna2 <?php
+        if ($cont % 2 == 0)
+            echo 'fr';
+        else
+            echo 'fl';
+        ?>" style="margin: 5px auto;">
+            <div style="width: 60%;">
+                <h2><?php echo $p->getNome(); ?></h2>
+                <small>
+                    <?php
+                    foreach ($categorias as $cat) {
+                        if ($cat->getId() == $p->getIdcategoria()) {
+                            echo $cat->getNome();
+                            break;
+                        }
                     }
-                }
-                ?>
-            </small>
-            <p class="color">R$<?php echo $p->getPreco(); ?></p>
-            <p><?php echo $p->getDescricao(); ?></p>
-        </div>
+                    ?>
+                </small>
+                <p class="color">R$<?php echo $p->getPreco(); ?></p>
+                <p><?php echo $p->getDescricao(); ?></p>
+            </div>
             <img src="" alt="<?php echo $p->getNome(); ?>" class="fr" style="margin: 10px; max-width: 40%; max-height: 100%;" />
             <div class="bb2"></div>
         </div>
+        
     <?php endforeach; ?>
+    <div class="clearfix"></div>
 </section>
 
