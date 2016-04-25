bf2b1e9ead7a6a69594c69b21e1b69eb066182b1
Guilherme Martins Panayotou <guilherme.it@live.com>
2016-04-25 15:35:26 -0300

    base pronta, falta apenas os detalhes...

--- class/bd/modelo/create-database-2016-04-08.sql
+++ class/bd/modelo/create-database-2016-04-08.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE  IF NOT EXISTS `widet532_animetoys` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `widet532_animetoys`;
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+USE `widet532_animetoys`;-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: widet532_animetoys
 -- ------------------------------------------------------
@@ -29,7 +28,7 @@ CREATE TABLE `categoria` (
   `nome` varchar(255) NOT NULL,
   `descricao` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +37,7 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (3,'Sem Categoria',''),(4,'Categoria 1',''),(5,'Categoria 2','');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,8 +56,10 @@ CREATE TABLE `cliente` (
   `email` varchar(255) DEFAULT NULL,
   `endereco` varchar(255) DEFAULT NULL,
   `pontos` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf_UNIQUE` (`cpf`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +68,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (2,'Guilherme Panayotou','42610183810','15996936586','guilherme.it@live.com','Av. General Carneiro, 1427',150);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +87,7 @@ CREATE TABLE `fornecedor` (
   `descricao` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +96,7 @@ CREATE TABLE `fornecedor` (
 
 LOCK TABLES `fornecedor` WRITE;
 /*!40000 ALTER TABLE `fornecedor` DISABLE KEYS */;
-INSERT INTO `fornecedor` VALUES (2,'fornecedor ','12312312','','<p>ewqeqweqweqw</p>\r\n',''),(4,'dsadas','','','',''),(5,'dasdasdsadsa','','','','');
+INSERT INTO `fornecedor` VALUES (1,'Sem Fornecedor','','','',''),(2,'fornecedor 1','','','',''),(3,'fornecedor 2','','','','');
 /*!40000 ALTER TABLE `fornecedor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,8 +141,8 @@ CREATE TABLE `produto` (
   `preco` float DEFAULT NULL,
   `idcategoria` tinyint(4) DEFAULT NULL,
   `estoque` int(11) DEFAULT NULL,
-  `idfornecedor` int(11) DEFAULT NULL,
-  `ativo` tinyint(4) NOT NULL DEFAULT '0',
+  `idfornecedor` int(11) DEFAULT '1',
+  `ativo` tinyint(4) NOT NULL,
   `img1` varchar(255) DEFAULT NULL,
   `img2` varchar(255) DEFAULT NULL,
   `img3` varchar(255) DEFAULT NULL,
@@ -147,7 +150,7 @@ CREATE TABLE `produto` (
   PRIMARY KEY (`id`),
   KEY `fk_produto_fornecedor_idx` (`idfornecedor`),
   CONSTRAINT `fk_produto_fornecedor` FOREIGN KEY (`idfornecedor`) REFERENCES `fornecedor` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,6 +159,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (11,'Produto 1','',50,3,74,1,1,'','','',''),(12,'Produto2 ','',0,4,0,1,1,'','','',''),(13,'produto 5','',0,5,0,3,0,'','','',''),(14,'Produto 3','<p>um grande presente para seus amigos</p>\r\n',25.5,5,30,3,1,'','','','');
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,10 +203,10 @@ DROP TABLE IF EXISTS `venda`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `venda` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `idproduto` int(11) DEFAULT NULL,
+  `idproduto` int(11) NOT NULL,
   `idcliente` int(11) DEFAULT NULL,
   `data` datetime DEFAULT NULL,
-  `desconto` decimal(10,0) DEFAULT NULL,
+  `desconto` float DEFAULT '0',
   `idusuario` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_venda_produto_idx` (`idproduto`),
@@ -211,7 +215,7 @@ CREATE TABLE `venda` (
   CONSTRAINT `fk_venda_cliente` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_venda_produto` FOREIGN KEY (`idproduto`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_venda_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=187 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -220,6 +224,7 @@ CREATE TABLE `venda` (
 
 LOCK TABLES `venda` WRITE;
 /*!40000 ALTER TABLE `venda` DISABLE KEYS */;
+INSERT INTO `venda` VALUES (170,11,NULL,'2016-04-20 06:05:22',0,1),(171,11,NULL,'2016-04-20 06:09:39',0,1),(172,11,NULL,'2016-04-20 06:10:09',0,1),(173,11,NULL,'2016-04-20 06:10:09',0,1),(174,11,NULL,'2016-04-20 06:10:09',0,1),(175,11,NULL,'2016-04-20 06:10:09',0,1),(176,11,NULL,'2016-04-20 06:10:09',0,1),(177,11,NULL,'2016-04-20 06:10:09',0,1),(178,11,NULL,'2016-04-20 06:10:09',0,1),(179,11,NULL,'2016-04-20 06:10:09',0,1),(180,11,NULL,'2016-04-20 06:10:09',0,1),(181,11,NULL,'2016-04-20 06:10:09',0,1),(182,11,2,'2016-04-20 06:12:45',0,1),(183,11,2,'2016-04-20 06:24:17',10,1),(184,14,2,'2016-04-20 17:25:44',10,1),(185,14,2,'2016-04-20 17:25:44',10,1),(186,14,2,'2016-04-20 17:25:44',10,1);
 /*!40000 ALTER TABLE `venda` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -232,4 +237,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-19 18:56:46
+-- Dump completed on 2016-04-20 12:32:12
