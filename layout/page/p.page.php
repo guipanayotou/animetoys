e2eb4232b7df81bb4aef9841165d9ab877ab11f0
gustavo <gustavo@192.168.25.207>
2016-05-18 00:49:33 -0300

    pronto

--- layout/page/p.page.php
+++ layout/page/p.page.php
@@ -2,8 +2,8 @@
 <section>
     <h1><i class="fa fa-ellipsis-h color"></i> <?php echo $produto->getNome(); ?></h1>
     <div class="bb2"></div>
-       <img src="./layout/img/produtos/<?php echo $produto->getImg1(); ?>" alt="<?php echo $produto->getNome(); ?>" class="fr" style="margin: 10px auto;  max-height: 250px; padding: 30px;" />
-             
+    <img src="./layout/img/produtos/<?php echo $produto->getImg1(); ?>" alt="<?php echo $produto->getNome(); ?>" class="fr" style="margin: 10px auto;  max-height: 250px; padding: 30px;" />
+
     <small>
         <?php
         foreach ($categorias as $cat) {
@@ -14,7 +14,12 @@
         }
         ?>
     </small>
-    <p class="color">R$ <?php echo str_replace('.', ',', $produto->getPreco()); ?></p>
+    <?php if ($produto->getPreco() > 0): ?>
+        <p class="color">R$ <?php echo str_replace('.', ',', $produto->getPreco()); ?></p>
+    <?php endif; ?>
+    <?php if ($produto->getPontos() > 0): ?>
+        <p><small>Pontos por compra:<b class="color"> <?php echo $produto->getPontos(); ?></b></small></p>
+    <?php endif; ?>
     <br /><br />
     <div>
         <?php echo $produto->getDescricao(); ?>
